03999741df87f28d7a84386e5e156fa493374d59
Panchenko Dmytro <panchenko0795@gmail.com>
2022-09-22 15:46:41 +0300

    Initial commit

---
+++ src/main/java/ua/nc/panchenko/lab2/NcMovieTicketOnlineServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.nc.panchenko.lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcMovieTicketOnlineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcMovieTicketOnlineServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/nc/panchenko/lab2/NcMovieTicketOnlineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nc.panchenko.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcMovieTicketOnlineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
